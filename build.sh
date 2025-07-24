73e7f72f3129cd3af301240acea180bdc2cdf18d
sinoserendipity <sinoserendipity@gmail.com>
2025-07-24 10:07:15 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -75,7 +75,9 @@ CUSTOM_PACKAGES=""
 
 #自添加插件 tailscale lucky
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-tailscale"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-tailscale-zh-cn"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-lucky"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-lucky-zh-cn"
 
 # ✅ 校验 CUSTOM_PACKAGES 中的包是否都存在于 packages_names.txt
 package_file="packages_names.txt"
